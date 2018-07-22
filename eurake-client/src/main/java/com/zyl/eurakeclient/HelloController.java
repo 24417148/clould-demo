4aae253e6109edf7b71f939e045127e563bf5ce4
zhengyoule <24417148@qq.com>
2018-07-22 23:52:15 +0800

    ribbo + service

--- eurake-client/src/main/java/com/zyl/eurakeclient/HelloController.java
+++ eurake-client/src/main/java/com/zyl/eurakeclient/HelloController.java
@@ -4,21 +4,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
 
   private final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+  private final RestTemplate restTemplate;
+
   @Autowired
-  private DiscoveryClient client;
+  public HelloController(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
 
   @RequestMapping(value = "/hello", method = RequestMethod.GET)
   public String index() {
-    return "hello world";
+
+    ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://db-service/world",
+        String.class);
+    return "hello" + responseEntity.getBody();
   }
 
 }
